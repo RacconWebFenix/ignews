b2fa01b8b7cb0c34150e52315afb40c5abec311b
Marcos César Domingues <marcos.domingues@gcbinvestimentos.com>
2022-06-28 08:07:09 -0300

    Testando Header

---
+++ src/components/Header/Header.spec.tsx
@@ -0,0 +1,29 @@
+import { render, screen } from "@testing-library/react";
+import { Header } from ".";
+
+jest.mock("next/router", () => {
+  return {
+    useRouter() {
+      return {
+        asPath: "/",
+      };
+    },
+  };
+});
+
+jest.mock("next-auth/react", () => {
+  return {
+    useSession() {
+      return [null, false, undefined];
+    },
+  };
+});
+
+describe("ActiveLink Component", () => {
+  it("renders correctly", () => {
+   render(<Header />);
+
+    expect(screen.getByText("Home")).toBeInTheDocument();
+    expect(screen.getByText("Posts")).toBeInTheDocument();
+  });
+});
