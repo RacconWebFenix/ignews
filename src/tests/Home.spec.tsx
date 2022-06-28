5d5367353d9dd7aaea384c94273ed06a48d18eb4
Marcos César Domingues <marcos.domingues@gcbinvestimentos.com>
2022-06-28 15:23:02 -0300

    Testando getStaticProps

--- src/tests/Home.spec.tsx
+++ src/tests/Home.spec.tsx
@@ -1,10 +1,12 @@
 import { render, screen } from "@testing-library/react";
 import { mocked } from "jest-mock";
 import { useSession } from "next-auth/react";
-import Home from "../pages";
+import Home, { getStaticProps } from "../pages";
+import { stripe } from "../services/stripe";
 
 jest.mock("next-auth/react");
 jest.mock("next/router");
+jest.mock("../services/stripe");
 
 describe("Home page", () => {
   it("render correctly", () => {
@@ -15,9 +17,30 @@ describe("Home page", () => {
       status: "unauthenticated",
     });
 
-
     render(<Home product={{ priceId: "fakeId", amount: "R$10,00" }} />);
 
     expect(screen.getByText("for R$10,00 month")).toBeInTheDocument();
+  });
+
+  it("loads initial data", async () => {
+    const retriveStripePriceMoked = mocked(stripe.prices.retrieve);
+
+    retriveStripePriceMoked.mockResolvedValueOnce({
+      id: "fakeId",
+      unit_amount: 1000,
+    } as any);
+
+    const response = await getStaticProps({});
+
+    expect(response).toEqual(
+      expect.objectContaining({
+        props: {
+          product: {
+            priceId: "fakeId",
+            amount: "$10.00",
+          },
+        },
+      })
+    );
   });
 });
